08b183113fcb4732227461f055b224f5cae298b7
a-shamasneh <a.ashmasneh@gmail.com>
2017-02-06 17:04:48 +0200

    ld

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express();
-var path    = require("path");
-app.use(express.static(__dirname + '/../client'));
+var path= require("path");
+var port = process.env.PORT || 3033;
+app.use(express.static(__dirname + '/client'));
 //console.log(__dirname)
 
-app.listen(3033);
+app.listen(port);
 
 console.log("Running at Port 3000",__dirname);
